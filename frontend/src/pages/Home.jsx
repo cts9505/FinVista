dba0eb32c650f99c1ba45f670c94a087829f4d10
Pratham-1229 <prathameshk2905@gmail.com>
2025-04-05 17:51:55 +0530

    home page

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Header from '../components/Header'
+import { FooterContainer } from '../components/Footer'
+const Home = () => {
+  return (
+    <>
+    <div className='flex flex-col items-center justify-center min-h-screen shadow-md bg-gradient-to-b from-gray-50 to bg-gray-100'>
+      <Navbar/>
+      <Header/>
+      
+    </div>
+    <FooterContainer/>
+    </>
+  )
+}
+
+export default Home
